9775ce99e9c0f69270c4ae33a0c474d69a24b84a
Alexis Haim <alexis@onvabosser.fr>
2020-04-23 14:14:18 +0400

    DB connection

--- app.py
+++ app.py
@@ -1,7 +1,19 @@
+import os
+
 from flask import Blueprint, Flask
+from flask_sqlalchemy import SQLAlchemy
+from dotenv import load_dotenv
 
 
+load_dotenv('.env')
+
+DB = SQLAlchemy()
 app = Flask(__name__)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ["SQLALCHEMY_DATABASE_URI"]
+app.config['ENV'] = os.getenv('ENV', 'dev')
+DB.init_app(app)
+
 
 home_bp = Blueprint('home', __name__)
 
