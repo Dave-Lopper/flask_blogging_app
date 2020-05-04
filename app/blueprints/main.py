72071657793f661da09711595d573d303f48c022
Alexis Haim <alexis@onvabosser.fr>
2020-05-04 10:56:06 +0400

    Sign-in and protected templates

--- app/blueprints/main.py
+++ app/blueprints/main.py
@@ -1,9 +1,19 @@
 # /app/blueprints/main.py
 from flask import Blueprint, render_template
 
-main = Blueprint('main', __name__)
+main = Blueprint("main", __name__)
 
 
-@main.route('/')
+@main.route("/")
 def index():
-    return render_template('index.j2.html')
+    return render_template("index.j2.html")
+
+
+@main.route("/signin")
+def signin():
+    return render_template("signin.j2.html")
+
+
+@main.route("/logged-in")
+def loggedin():
+    return render_template("logged-in.j2.html")
