12bdd17ef923a9a5f505ced1342cc96a3c7a83e9
Alexis Haim <alexis@onvabosser.fr>
2020-05-28 10:06:15 +0400

    Post writing form

--- test/functional/test_index_page.py
+++ test/functional/test_index_page.py
@@ -7,6 +7,7 @@ def test_index_page_unlogged(test_client):
     WHEN index is hit
     THEN - Template is rendered correctly
          - Login form is included
+         - Post form is not included
     """
     response = test_client.get("/")
     assert response.status_code == 200
@@ -17,6 +18,9 @@ def test_index_page_unlogged(test_client):
     assert "Password" in decoded_response
     assert "Remember me ?" in decoded_response
     assert "Not with us yet ?" in decoded_response
+    assert "Logout" not in decoded_response
+    assert "How do you feel like today ?" not in decoded_response
+    assert "Post" not in decoded_response
 
 
 def test_index_page_logged(test_client, db_init, insert_user, login_user):
@@ -25,6 +29,7 @@ def test_index_page_logged(test_client, db_init, insert_user, login_user):
     WHEN index is hit
     THEN - Template is rendered correctly
          - Login form not included
+         - Post form is included
     """
     response = test_client.get("/")
     assert response.status_code == 200
